5fe035f8b374a2aae56eeab644b5267a369d5969
Deepak Ramani <deepakramani65@gmail.com>
2024-09-25 21:23:24 +0530

    nav links fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,7 +41,7 @@ const NavBar = () => {
         {links.map(({id,link})=>(
         <li key={id} className="px-4 cursor-pointer capitalize font-medium text-xl
              text-gray-500 hover:scale-105 duration-200">
-          <Link to={link} smooth duration={500}>
+          <Link to={link} smooth="true" duration={500}>
             {link}
             {console.log(link)}
             </Link>
